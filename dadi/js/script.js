b24a390ec2ce7184fa503c479ecdbe55d18ffb84
Jac22 <jacopo.prudentino@gmail.com>
2022-11-10 14:55:11 +0100

    Aggiunta animazione ai dadi

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -3,9 +3,68 @@ const cDice= document.getElementById("cpu");
 const w= document.getElementById("w");
 const p= document.getElementById("p");
 const l= document.getElementById("l");
+let k=true;
+document.querySelector("button").addEventListener("mouseover",
+    function(){
+        k=true;
+        let id = null;
+        clearInterval(id);
+        id = setInterval(frame, 60);
+        function frame() {
+            if (k==false) {
+                clearInterval(id);
+            }
+            else {
+                uDice.classList.remove("d1","d2","d3","d4","d5","d6");
+                cDice.classList.remove("d1","d2","d3","d4","d5","d6");
+
+                let uN=Math.floor(Math.random() *6) + 1;
+                let cN=Math.floor(Math.random() *6) + 1;
+
+                switch(uN){
+                    case 1: uDice.classList.add("d1");
+                    break;
+                    case 2: uDice.classList.add("d2");
+                    break;
+                    case 3: uDice.classList.add("d3");
+                    break;
+                    case 4: uDice.classList.add("d4");
+                    break;
+                    case 5: uDice.classList.add("d5");
+                    break;
+                    case 6: uDice.classList.add("d6");
+                    break;
+                }
+
+
+                switch(cN){
+                    case 1: cDice.classList.add("d1");
+                    break;
+                    case 2: cDice.classList.add("d2");
+                    break;
+                    case 3: cDice.classList.add("d3");
+                    break;
+                    case 4: cDice.classList.add("d4");
+                    break;
+                    case 5: cDice.classList.add("d5");
+                    break;
+                    case 6: cDice.classList.add("d6");
+                    break;
+                }
+            }
+        }
+    }
+);
+
+document.querySelector("button").addEventListener("mouseout",
+function(){
+    k=false;
+}
+);
+
 document.querySelector("button").addEventListener("click",
     function(){
-
+        k=false;
         uDice.classList.remove("d1","d2","d3","d4","d5","d6");
         cDice.classList.remove("d1","d2","d3","d4","d5","d6");
 
