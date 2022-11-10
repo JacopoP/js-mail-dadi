7d36830a480c9d5d093d68a8860efe4792d1cab3
Jac22 <jacopo.prudentino@gmail.com>
2022-11-10 14:14:21 +0100

    Completato esrcizio dadi

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,5 +1,5 @@
 const uDice= document.getElementById("user");
-const cDice= document.getElementById("user");
+const cDice= document.getElementById("cpu");
 const w= document.getElementById("w");
 const p= document.getElementById("p");
 const l= document.getElementById("l");
@@ -20,16 +20,48 @@ document.querySelector("button").addEventListener("click",
         }
 
         let uN=Math.floor(Math.random() *6) + 1;
-        let uC=Math.floor(Math.random() *6) + 1;
-        console.log(uN, uC)
+        let cN=Math.floor(Math.random() *6) + 1;
 
-        if(uN>uC){
+        // cambio numero dado user
+        switch(uN){
+            case 1: uDice.classList.add("d1");
+            break;
+            case 2: uDice.classList.add("d2");
+            break;
+            case 3: uDice.classList.add("d3");
+            break;
+            case 4: uDice.classList.add("d4");
+            break;
+            case 5: uDice.classList.add("d5");
+            break;
+            case 6: uDice.classList.add("d6");
+            break;
+        }
+
+        // cambio numero dado cpu
+        switch(cN){
+            case 1: cDice.classList.add("d1");
+            break;
+            case 2: cDice.classList.add("d2");
+            break;
+            case 3: cDice.classList.add("d3");
+            break;
+            case 4: cDice.classList.add("d4");
+            break;
+            case 5: cDice.classList.add("d5");
+            break;
+            case 6: cDice.classList.add("d6");
+            break;
+        }
+
+        // risultato
+        if(uN>cN){
             w.classList.remove("hide")
         }
-        else if(uN<uC){
+        else if(uN<cN){
             l.classList.remove("hide")
         }
-        else if(uN==uC){
+        else if(uN==cN){
             p.classList.remove("hide")
         }
     }
